8275ade988c5b4f3ed05a293313ab18c95c81a7a
Ilya Fursa <fursa.cs@gmail.com>
2018-06-26 16:24:52 +0400

    Simple server and requests handler

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,6 +1,18 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class AppMain {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws Exception {
+        FrontendServlet frontendServlet = new FrontendServlet();
+
+        Server server = new Server(8080);
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(handler);
+        handler.addServlet(new ServletHolder(frontendServlet), "/authform");
+
+        server.start();
+        server.join();
     }
 }
